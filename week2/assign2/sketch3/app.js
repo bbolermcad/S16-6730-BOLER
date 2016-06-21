5fbf79753d87b99f520b2a4f8c0237b5362e21ba
Brigit Boler <bboler@mcad.edu>
2016-06-20 20:48:54 -0500

    this is an unholy abomination

--- week2/assign2/sketch3/app.js
+++ week2/assign2/sketch3/app.js
@@ -1,22 +1,49 @@
-int option = 1;
- function setup() {
-  size(400, 300);
-  smooth();
-  noFill();
-}
+var x;
+var num = 50;
+var y;
+var num= 25;
 
-draw() {
-  background(255);
+function setup() {
 
-    // Option 3: Overlapping circles
-    for (int x = 50; x <= width-50; x += 20) {
-      for (int y = 50; y <= height-50; y+=20) {
-        ellipse(x, y, 40, 40);
-      }
-    }
+  createCanvas(720, 360);
+  background(59,89,152);
+  noStroke();
+
+  // Draw white bars
+  fill(255);
+  x = 40;
+  y= 20;
+  for(var i = 0; i < num/3; i++) {
+    ellipse(y, x, 50, 50);
+    x+=55;
+    y+=100;
   }
 
-void mousePressed() {
-  option++;
-  if (option > 5) option = 1;
+  // Gray bars
+  fill(51);
+  x = 40;
+  y= 40;
+  for(var i = 0; i < num; i++) {
+    ellipse(y, x, 10, 10);
+    x += 20;
+    y =+ 250;
+  }
+
+  x= 50;
+  y= 700;
+  for(var i = 0; i < num; i++) {
+    ellipse(y, x, 30, 30);
+    x += 40;
+    x += 40;
+  }
+
+  // Thin lines
+  x = 45;
+  y= 55;
+  fill(0);
+  for(var i = 0; i < num-1; i++) {
+    ellipse(y, x, 40, 40);
+    x+= 40;
+    y+= 90;
+  }
 }
