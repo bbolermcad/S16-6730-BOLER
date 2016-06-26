a05af1efd47b5674fde77cd535bbd747124a4ae7
Brigit Boler <bboler@mcad.edu>
2016-06-26 17:13:14 -0500

    copying the example directly
    wow it still doesn’t work shocking

--- week3/assign3-v1/app.js
+++ week3/assign3-v1/app.js
@@ -1,35 +1,58 @@
 var canvasWidth = 600
-var canvasHeight = 400
+var canvasHeight = 500
 
+// where I downloaded the Origin of the Species raw text file
 var url = 'darwin.txt'
 
+// where we'll put all the words
 var world = $('#world')
 
-$.get(url, fuction(data) {
+// the $.get here is a jQuery 'AJAX' function
+// that will ask the webserver over the network for the
+// file at . I supply it with a function that is called
+// when the data is retreived
+$.get(url, function(data) {
 
+  // split when called on a string,
+  // will make an array of the separated pieces
   var words = data.split(' ')
-  words.forEach(function(word)
-  var span = $(' ').text( '_' + word + '_')
-  span.attr('data-word', word)
 
-  span.on('mouseover', function(e){
-  $(this).css('font-weight', 'bold')
+  // for each word in the text...
+  words.forEach(function(word) {
+
+    // make it a span so I can do stuff with it
+    // think of this as a 'handle'
+    var span = $('').text( ' ' + word + ' ')
+
+    // mark the span with 'data-word' so I can look it up easily later
+    span.attr('data-word', word)
+
+    // highlight the word when I hover over
+    span.on('mouseover', function(e) {
+      $(this).css('font-weight', 'bold')
+    })
+
+    span.on('mouseout', function(e) {
+      $(this).css('font-weight', 'normal')
+    })
+
+    // when you click on a word..
+    span.on('click', function(e) {
+
+      // make it stand out:
+      $(this).fadeOut('slow')
+      var target = $(this).attr('data-word')
+
+      // find all the other instances of that word
+      // and make them stand out
+      $('span[data-word=' + target + ']')
+        $(this).fadeOut('slow')
+
+    })
+
+    // add that span to our container div
+    world.append(span)
+
   })
-
-  span.on('mouseout', function(e)
-  $(this).css('font-weight', "normal")
-  })
-
-  span.on('click', function(e) {
-    $(this).fadeOut("slow")
-    var target = $(this).attr('data-word')
-
-    $('span[data-word=' + target +']')
-     .fadeOut("slow")
-  })
-
-  world.append(span)
-
-})
 
 })
