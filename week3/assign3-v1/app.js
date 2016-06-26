f508493acee83a804e8b7f5efc1ba7623558aed7
Brigit Boler <bboler@mcad.edu>
2016-06-26 15:13:45 -0500

    finishing up my version

--- week3/assign3-v1/app.js
+++ week3/assign3-v1/app.js
@@ -11,4 +11,25 @@ $.get(url, fuction(data) {
   words.forEach(function(word)
   var span = $(' ').text( '_' + word + '_')
   span.attr('data-word', word)
-}
+
+  span.on('mouseover', function(e){
+  $(this).css('font-weight', 'bold')
+  })
+
+  span.on('mouseout', function(e)
+  $(this).css('font-weight', "normal")
+  })
+
+  span.on('click', function(e) {
+    $(this).fadeOut("slow")
+    var target = $(this).attr('data-word')
+
+    $('span[data-word=' + target +']')
+     .fadeOut("slow")
+  })
+
+  world.append(span)
+
+})
+
+})
