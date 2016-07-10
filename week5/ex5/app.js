44bf8ba8c1306175e3b39b733492b2d2bd1c1589
Brigit Boler <bboler@mcad.edu>
2016-07-10 18:15:29 -0500

    messed up which app.js i was using

--- week5/ex5/app.js
+++ week5/ex5/app.js
@@ -2,8 +2,8 @@ var beats, sound1, sound2;
 
 function preload() {
   beats = loadSound('beats.wav');
-  sound1 = loadSound('guitar.wav')
-  sound2 = loadSound('synth.mp3')
+  sound1 = loadSound('guitar.wav');
+  sound2 = loadSound('synth.mp3');
 }
 
 function setup() {
