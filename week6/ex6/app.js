0067ced220b34757cc3dc9ae327a38f8a7a58a60
Brigit Boler <bboler@mcad.edu>
2016-07-14 23:47:31 -0500

    fixed the problems

--- week6/ex6/app.js
+++ week6/ex6/app.js
@@ -5,7 +5,7 @@ createCanvas (600, 600);
 angleMode(DEGREES);
 
 pos = 0.0;
-inc = 6.0;
+inc = 4.0;
 
 }
 
@@ -15,11 +15,11 @@ function draw() {
   translate(width/2, height/2);
 
   var mySinVal = sin(pos);
-  amplified = mySinVal * 100;
+  amplified = mySinVal * 10;
 
-  for (x = 0, x <= 360, x = x + 20); {
+  for (x = 0; x <= 360; x = x + 10); {
     fill(101, 36, 175);
-    ellipse(0, 100, 10, 10);
+    ellipse(0, 100, 20, 20);
     rotate(x);
 }
 
