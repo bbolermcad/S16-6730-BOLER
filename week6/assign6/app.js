99646c5b2cb2dc9b4ef008211377fbf7bbe9d7c3
Brigit Boler <bboler@mcad.edu>
2016-07-17 11:53:57 -0500

    assignment 6

---
+++ week6/assign6/app.js
@@ -0,0 +1,20 @@
+var x, y;
+
+function setup() {
+  createCanvas(500, 400);
+  background(200);
+  y = height;
+  x = width/2;
+}
+
+function draw(){
+  stroke(14, 181, 215)
+  fill(14, 181, 215);
+  ellipse (x, y, 10, 10);
+
+  y = y + 1;
+
+  if (y > 500){
+    y = height;
+  }
+}
