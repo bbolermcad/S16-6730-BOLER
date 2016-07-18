7ec6d042413bbe6b8df137eaeead92f434772087
Brigit Boler <bboler@mcad.edu>
2016-07-17 22:44:45 -0500

    finished!

--- week6/assign6/app.js
+++ week6/assign6/app.js
@@ -11,10 +11,15 @@ function setup() {
   function draw(){
   stroke(14, 181, 215)
   fill(14, 181, 215);
-  ellipse (x, y, 10, 10);
+
+  ellipse (x*0.5, y, 10, 10);
+  ellipse (x, y*1.2, 10, 10);
+  ellipse (x*.7, y*0.7, 10, 10);
+  ellipse (x, y*1, 10, 10);
+  ellipse (x*.2, y*0.9, 10, 10);
+  ellipse (x*.5, y*1, 10, 10);
+  ellipse (x*9, y*0.9, 10, 10);
 
   y = y + 1;
   x = x + random(-1, 1);
-
-  for (y = 0; y >= 1; x = x + 1) 
 }
