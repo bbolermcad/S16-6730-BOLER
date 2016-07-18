b92c5a6601a8b726fe47d71b55deb2bfbc3a81bb
Brigit Boler <bboler@mcad.edu>
2016-07-17 19:09:48 -0500

    adding a loop

--- week6/assign6/app.js
+++ week6/assign6/app.js
@@ -3,16 +3,18 @@ var x, y;
 function setup() {
   createCanvas(500, 400);
   background(200);
-  y = height;
+  y = 0;
   x = width/2;
 }
 
-function draw(){
+
+  function draw(){
   stroke(14, 181, 215)
   fill(14, 181, 215);
   ellipse (x, y, 10, 10);
 
-  y = y - 1;
+  y = y + 1;
   x = x + random(-1, 1);
 
+  for (y = 0; y >= 1; x = x + 1) 
 }
